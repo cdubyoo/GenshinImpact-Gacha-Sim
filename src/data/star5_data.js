c372138b63501aef7dfe845c333b3615022010c1
cdubyoo <chungwongg@gmail.com>
2020-10-24 08:16:17 -0700

    finished 5 star data

--- src/data/star5_data.js
+++ src/data/star5_data.js
@@ -2,7 +2,76 @@ export default [
     {
         'id': 1,
         'type' : 'character',
+        'name' : 'Keqing'
+    },
+    {
+        'id': 2,
+        'type' : 'character',
         'name' : 'Qiqi'
     },
-    
+    {
+        'id': 3,
+        'type' : 'character',
+        'name' : 'Jean'
+    },
+    {
+        'id': 4,
+        'type' : 'character',
+        'name' : 'Mona'
+    },
+    {
+        'id': 5,
+        'type' : 'character',
+        'name' : 'Diluc'
+    },
+    {
+        'id': 6,
+        'type' : 'weapon',
+        'name' : 'Skyward Harp'
+    },
+    {
+        'id': 7,
+        'type' : 'weapon',
+        'name' : 'Skyward Atlas'
+    },
+    {
+        'id': 8,
+        'type' : 'weapon',
+        'name' : 'Skyward Spine'
+    },
+    {
+        'id': 9,
+        'type' : 'weapon',
+        'name' : 'Skyward Pride'
+    },
+    {
+        'id': 10,
+        'type' : 'weapon',
+        'name' : 'Aquila Favonia'
+    },
+    {
+        'id': 11,
+        'type' : 'weapon',
+        'name' : 'Amos Bow'
+    },
+    {
+        'id': 12,
+        'type' : 'weapon',
+        'name' : 'Lost Prayer To The Sacred Winds'
+    },
+    {
+        'id': 13,
+        'type' : 'weapon',
+        'name' : 'Primordial Jade Winged-Spear'
+    },
+    {
+        'id': 14,
+        'type' : 'weapon',
+        'name' : "Wolf's Gravestone"
+    },
+    {
+        'id': 15,
+        'type' : 'weapon',
+        'name' : 'Skyward Blade'
+    },
 ]

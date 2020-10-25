51dac498bfcbfc5f9ef2094fd5a9ef67af3d2662
chung <chungwongg@gmail.com>
2020-10-24 19:24:07 -0700

    updated 3 and 4 star data

---
+++ src/data/3star.js
@@ -0,0 +1,67 @@
+export default [
+    {
+        'type': 'weapon',
+        'name': 'Slingshot',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Raven Bow',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Thrilling Tales Of Dragon Slayers',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Black Tassel',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Bloodtainted Greatsword',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Skyrider Sword',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Cool Steel',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': "Sharpshooter's Oath",
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Emerald Orb',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Magic Guide',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Debate Club',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Ferrous Shadow',
+        'image': ''
+    },
+    {
+        'type': 'weapon',
+        'name': 'Harbinger Of Dawn',
+        'image': ''
+    }
+]
---
+++ src/data/4star.js
@@ -0,0 +1,163 @@
+export default [
+    {
+        'type': 'character',
+        'name': 'Sucrose',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Noelle',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Fischl',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Xingqiu',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Xiangling',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Razor',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Barbara',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Chongyun',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Bennett',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Ningguang',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Beidou',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Amber',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Kaeya',
+        'image': ''
+    },
+    {
+        'type': 'character',
+        'name': 'Lisa',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Rust',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'The Stringless',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Eye of Perception',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'The Widsith',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Favonius Lance',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Rainslasher',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'The Bell',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Lions Roar',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'The Flute',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Sacrificial Bow',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Favonius Warbow',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Sacrificial Fragments',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Favonius Codex',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Sacrificial Greatsword',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Favonius Greatsword',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Sacrificial Sword',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': 'Favonius Sword',
+        'image': ''
+    },
+    {
+        'type':  'weapon',
+        'name': "Dragon's Bane",
+        'image': ''
+    },
+
+]

5a3ff21b41d41686e9f419078e39c1f29b19c6d2
Kevin Diec <kevdiec@gmail.com>
2020-10-26 22:23:05 -0700

    rolls

--- src/components/Rolls.js
+++ src/components/Rolls.js
@@ -9,7 +9,11 @@ export default function Rolls(props) {
     const [roll, setRoll] = useState(null)
     const [tenPityCount, setTenPityCount] = useState(0)
     const [ninetyPityCount, setNinetyPityCount] = useState(0)
+    const [rollCount, setRollCount] = useState(0)
 
+    useEffect(() => {
+        handleNRolls(rollCount)
+    }, [rollCount])
 
     // handler for single roll
     const handleSingleRoll = () => {
@@ -57,6 +61,56 @@ export default function Rolls(props) {
             console.log('obtained a 3 star, pity counter ++')
         }
     }
+
+    const handleNRolls = () => {
+        if (rollCount === 0) {
+            return
+        }
+        // sets the main rolling RNG
+        const rng = Math.floor(Math.random() * 1000) +1
+        // pulls a random item from the data set for each star rating
+        const randomFiveStar = Math.floor(Math.random() * fiveStarData.length) 
+        const randomFourStar = Math.floor(Math.random() * fourStarData.length)
+        const randomThreeStar = Math.floor(Math.random() * threeStarData.length)
+        
+        // check if ten pity count has hit
+        if (tenPityCount === 9) {
+            setRoll(fourStarData[randomFourStar].name)
+            setTenPityCount(0)
+            console.log('obtained a 4 star through pity')
+            return;
+        }
+
+        // check if 90 pity count has hit
+        if (ninetyPityCount === 89) {
+            setRoll(fiveStarData[randomFiveStar].name)
+            setNinetyPityCount(0)
+            console.log('obtained a 5 star through pity')
+            return;
+        } 
+
+        // check if rng hit 5 star which is 0.6%
+        if (rng <= 6) {
+            setRoll(fiveStarData[randomFiveStar].name)
+            setNinetyPityCount(0)
+            console.log('obtained a 5 star, resetting pity')
+        // check if rng hit 4 star which is 5.1%
+        } else if (rng <= 51) {
+            setRoll(fourStarData[randomFourStar].name)
+            setTenPityCount(0)
+            // only increment for 90 pity counter b/c 10 pity resets upon hitting 4 star
+            setNinetyPityCount(prevState => prevState +1)
+            console.log('obtained a 4 star, resetting pity')
+        // anything else is a 3 star
+        } else {
+            setRoll(threeStarData[randomThreeStar].name)
+            // pity counter increment for both
+            setTenPityCount(prevState => prevState + 1)
+            setNinetyPityCount(prevState => prevState +1)
+            console.log('obtained a 3 star, pity counter ++')
+        }
+        setRollCount(rollCount - 1)
+    }
     
     const handleTenRoll = () => {
         for (let i = 0; i < 10; i++) {
@@ -64,12 +118,16 @@ export default function Rolls(props) {
         }
     }
     
+    const handleNRoll = () => {
+        setRollCount(10)
+    }
     return (
         <>
         <div>
             <span>
                 <button onClick={handleSingleRoll} className='btn btn-primary mr-2'>Wish x1</button>
                 <button onClick={handleTenRoll} className='btn btn-primary mr-2'>Wish x10</button>
+                <button onClick={handleNRoll} className='btn btn-primary mr-2'>Wish xN</button>
                 <button className='btn btn-danger'>Reset</button>
             </span>
         </div>

9e462628b90b4c3d57ed8634b04c7d443f023ffe
cdubyoo <chungwongg@gmail.com>
2020-10-30 06:50:30 -0700

    finished inventory, cleaned up trash

--- src/components/Rolls.js
+++ src/components/Rolls.js
@@ -6,10 +6,6 @@ import threeStarData from '../data/3star'
 
 
 export default function Rolls() {
-    
-    // roll state
-    const [roll, setRoll] = useState()
-    
     // pity count state
     const [tenPityCount, setTenPityCount] = useState(0)
     const [ninetyPityCount, setNinetyPityCount] = useState(0)
@@ -36,19 +32,27 @@ export default function Rolls() {
             </figure>
         </div>
     )
+    
 
     // render out inventory list
     const inventoryItem = inventory.map((item) => 
+
+        item.type === 'character' || item.rating === '5' ? 
         <div>
-            {item.name}
+            {item.type ==='character' ? <strong>{item.name}</strong> : <span>{item.name}</span> } 
+            <svg width="1em" height="1em" viewBox="0 0 16 16" className='pb-1 pl-2 pr-1' fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 0a1 1 0 0 1 1 1v5.268l4.562-2.634a1 1 0 1 1 1 1.732L10 8l4.562 2.634a1 1 0 1 1-1 1.732L9 9.732V15a1 1 0 1 1-2 0V9.732l-4.562 2.634a1 1 0 1 1-1-1.732L6 8 1.438 5.366a1 1 0 0 1 1-1.732L7 6.268V1a1 1 0 0 1 1-1z"/>
+            </svg> 
+            {item.quantity}
             <span className="float-right">
-            {item.rating}
-            
+            {item.rating ==='5' ? <strong>{item.rating}</strong>:<span>{item.rating}</span>}
             <svg width="1em" height="1em" viewBox="0 0 16 16" className=" pb-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
             </svg>
             </span>
         </div>
+        :
+        null
     )
        
     // calls function everytime rollCount changes, decrement each time from 10
@@ -76,55 +80,121 @@ export default function Rolls() {
         
         // check if ten pity count has hit
         if (tenPityCount === 9) {
-            setRoll(fourStarData[randomFourStar].name)
             setTenPityCount(0)
-            console.log('obtained a 4 star through pity')
             setCurrentRolls(prevState => [...prevState, newFourStar])
             setTotalRolls(prevState => prevState +1)
+            // checks to see if item already in inventory
+            const exists = inventory.find(item => item.name === newFourStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newFourStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newFourStar])
+                }
             return;
         } 
 
         // check if 90 pity count has hit
         if (ninetyPityCount === 89) {
-            setRoll(fiveStarData[randomFiveStar].name)
             setNinetyPityCount(0)
-            console.log('obtained a 5 star through pity')
             setCurrentRolls(prevState => [...prevState, newFiveStar])
             setTotalRolls(prevState => prevState +1)
+            const exists = inventory.find(item => item.name === newFiveStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newFiveStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newFiveStar])
+                }
+
             return;
         } 
 
         // check if rng hit 5 star which is 0.6%
         if (rng <= 6) {
-            setRoll(fiveStarData[randomFiveStar].name)
             setNinetyPityCount(0)
-            console.log('obtained a 5 star, resetting pity')
+            setCurrentRolls(prevState => [...prevState, newFiveStar])
+            // checks to see if item already in inventory
+            const exists = inventory.find(item => item.name === newFiveStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newFiveStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newFiveStar])
+                }
+
         // check if rng hit 4 star which is 5.1%
         } else if (rng <= 51) {
-            setRoll(fourStarData[randomFourStar].name)
             setTenPityCount(0)
             // only increment for 90 pity counter b/c 10 pity resets upon hitting 4 star
             setNinetyPityCount(prevState => prevState +1)
-            console.log('obtained a 4 star, resetting pity')
+            setCurrentRolls(prevState => [...prevState, newFourStar])
+            // checks to see if item already in inventory
+            const exists = inventory.find(item => item.name === newFourStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newFourStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newFourStar])
+                }
+
         // anything else is a 3 star
         } else {
-            setRoll(threeStarData[randomThreeStar].name)
             // pity counter increment for both
             setTenPityCount(prevState => prevState + 1)
             setNinetyPityCount(prevState => prevState +1)
-            console.log('obtained a 3 star, pity counter ++')
+            setCurrentRolls(prevState => [...prevState, newThreeStar])
+
+            // checks to see if item already in inventory
+            const exists = inventory.find(item => item.name === newThreeStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newThreeStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newThreeStar])
+                }
         } 
-        setCurrentRolls(prevState => [...prevState, newThreeStar])
         setTotalRolls(prevState => prevState +1)
-        
-        const exists = inventory.find(p => p.name === threeStarData[randomThreeStar].name)
-        if (exists) return
-        else {
-            setInventory(prevState => [...prevState, newThreeStar])
-        }
-    
-  
-        // setInventory(prevState => [...prevState, newThreeStar])
     }
 
     
@@ -154,47 +224,114 @@ export default function Rolls() {
         
         // check if ten pity count has hit
         if (tenPityCount === 9) {
-            setRoll(fourStarData[randomFourStar].name)
             setTenPityCount(0)
-            console.log('obtained a 4 star through pity')
             setRollCount(rollCount - 1)
             setCurrentRolls(prevState => [...prevState, newFourStar])
             setTotalRolls(prevState => prevState +1)
+            const exists = inventory.find(item => item.name === newFourStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newFourStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newFourStar])
+                }
             return;
         }
 
         // check if 90 pity count has hit
         if (ninetyPityCount === 89) {
-            setRoll(fiveStarData[randomFiveStar].name)
             setNinetyPityCount(0)
-            console.log('obtained a 5 star through pity')
             setRollCount(rollCount - 1)
             setCurrentRolls(prevState => [...prevState, newFiveStar])
             setTotalRolls(prevState => prevState +1)
+            const exists = inventory.find(item => item.name === newFiveStar.name)
+            if (exists) {
+                // checks for the index number of duplicate item
+                const index = inventory.findIndex(item => item.name === newFiveStar.name)
+                // makes a copy of inventory
+                const updatedInventory = [...inventory]
+                // updates object quantity count
+                updatedInventory[index].quantity ++
+                // replace inventory with updated one
+                setInventory(updatedInventory)
+            }
+            // add item to inventory if not duplicate
+            else {
+                setInventory(prevState => [...prevState, newFiveStar])
+            }
             return;
         } 
 
         // check if rng hit 5 star which is 0.6%
         if (rng <= 6) {
-            setRoll(fiveStarData[randomFiveStar].name)
             setNinetyPityCount(0)
             setCurrentRolls(prevState => [...prevState, newFiveStar])
-            console.log('obtained a 5 star, resetting pity')
+            const exists = inventory.find(item => item.name === newFourStar.name)
+            if (exists) {
+                // checks for the index number of duplicate item
+                const index = inventory.findIndex(item => item.name === newFourStar.name)
+                // makes a copy of inventory
+                const updatedInventory = [...inventory]
+                // updates object quantity count
+                updatedInventory[index].quantity ++
+                // replace inventory with updated one
+                setInventory(updatedInventory)
+            }
+            // add item to inventory if not duplicate
+            else {
+                setInventory(prevState => [...prevState, newFourStar])
+            }
         // check if rng hit 4 star which is 5.1%
         } else if (rng <= 51) {
-            setRoll(fourStarData[randomFourStar].name)
             setTenPityCount(0)
             // only increment for 90 pity counter b/c 10 pity resets upon hitting 4 star
             setNinetyPityCount(prevState => prevState +1)
             setCurrentRolls(prevState => [...prevState, newFourStar])
-            console.log('obtained a 4 star, resetting pity')
+            const exists = inventory.find(item => item.name === newFourStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newFourStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newFourStar])
+                }
         // anything else is a 3 star
         } else {
-            setRoll(threeStarData[randomThreeStar].name)
             setTenPityCount(prevState => prevState + 1)
             setNinetyPityCount(prevState => prevState +1)
-            console.log('obtained a 3 star, pity counter ++')
             setCurrentRolls(prevState => [...prevState, newThreeStar])
+
+            // checks to see if item already in inventory
+            const exists = inventory.find(item => item.name === newThreeStar.name)
+                if (exists) {
+                    // checks for the index number of duplicate item
+                    const index = inventory.findIndex(item => item.name === newThreeStar.name)
+                    // makes a copy of inventory
+                    const updatedInventory = [...inventory]
+                    // updates object quantity count
+                    updatedInventory[index].quantity ++
+                    // replace inventory with updated one
+                    setInventory(updatedInventory)
+                }
+                // add item to inventory if not duplicate
+                else {
+                    setInventory(prevState => [...prevState, newThreeStar])
+                }
         }
         setRollCount(rollCount - 1)
         setTotalRolls(prevState => prevState +1)
@@ -207,13 +344,10 @@ export default function Rolls() {
         setRollCount(10)
     }
 
-    // reset all state
+    // reset the page
     const handleReset = () => {
-        setRoll(null)
-        setTenPityCount(0)
-        setNinetyPityCount(0)
-        setCurrentRolls([])
-        setInventory([])
+        window.location.reload()
+        
     }
 
     return (
@@ -232,7 +366,7 @@ export default function Rolls() {
                         <br></br>
                         Ninety Pity Count = {ninetyPityCount}
                         <br></br>
-                            Current Roll = {roll}
+                        Wish Results:
                         <div className="container">
                             <div className="row">
                                 {rollResults}
--- src/data/4star.js
+++ src/data/4star.js
@@ -3,193 +3,225 @@ export default [
         'type': 'character',
         'name': 'Sucrose',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Sucrose.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Noelle',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Noelle.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Fischl',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Fischl.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Xingqiu',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Xingqiu.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Xiangling',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Xiangling.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Razor',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Razor.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Barbara',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Barbara.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Chongyun',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Chongyun.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Bennett',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Bennett.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Ningguang',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Ningguang.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Beidou',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Beidou.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Amber',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Amber.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Kaeya',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Kaeya.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type': 'character',
         'name': 'Lisa',
         'image': 'https://rerollcdn.com/GENSHIN/Characters/Lisa.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Rust',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Rust.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'The Stringless',
         'image': "https://rerollcdn.com/GENSHIN/Weapon/NEW/The_Stringless.png",
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Eye of Perception',
         'image': 'https://gi-sim.s3-us-west-1.amazonaws.com/Weapon_Eye_of_Perception.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'The Widsith',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/The_Widsith.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Favonius Lance',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Favonius_Lance.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Rainslasher',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Rainslasher.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'The Bell',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/The_Bell.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Lions Roar',
         'image': "https://rerollcdn.com/GENSHIN/Weapon/NEW/Lion's_Roar.png",
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'The Flute',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/The_Flute.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Sacrificial Bow',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Sacrificial_Bow.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Favonius Warbow',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Favonius_Warbow.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Sacrificial Fragments',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Sacrificial_Fragments.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Favonius Codex',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Favonius_Codex.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Sacrificial Greatsword',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Sacrificial_Greatsword.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Favonius Greatsword',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Favonius_Greatsword.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Sacrificial Sword',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Sacrificial_Sword.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': 'Favonius Sword',
         'image': 'https://rerollcdn.com/GENSHIN/Weapon/NEW/Favonius_Sword.png',
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
     {
         'type':  'weapon',
         'name': "Dragon's Bane",
         'image': "https://rerollcdn.com/GENSHIN/Weapon/NEW/Dragon's_Bane.png",
-        'rating': '4'
+        'rating': '4',
+        'quantity': 1
     },
 
 ]
